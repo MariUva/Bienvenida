9928a62da8318c27afbffaa86adf62cc1254ff25
MarianaU <mariana.urreav@uqvirtual.edu.co>
2023-09-01 21:26:44 -0500

    Primera version del proyecto

---
+++ src/main/java/co/uniquindio/saludo/SaludoApplication.java
@@ -0,0 +1,11 @@
+package co.uniquindio.saludo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SaludoApplication.class, args);
+    }
+}
---
+++ src/main/java/co/uniquindio/saludo/modelo/Cliente.java
@@ -0,0 +1,35 @@
+package co.uniquindio.saludo.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+
+    @Id private String cedula;
+
+    private String nombre;
+    private String email;
+    private String telefono;
+
+    public Cliente() {
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    
+}
